546881123f9dd3c6e75607dc89ad83e6047d1d2a
Jangmyun <cndlsrb2739@gmail.com>
2025-08-14 20:20:58 +0900

    set default app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import type { Tile, Display } from "./types";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Display resolution
+  const [display, setDisplay] = useState<Display>({
+    width: 1920,
+    height: 1080,
+  });
+
+  // Tiles
+  const [tiles, setTiles] = useState<Tile[]>([]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="container">
+        <h1>PI Wall Generator</h1>
+        {/* DISPLAY RESOLUTION INPUTS */}
+        <div id="display-resolution">
+          <input type="number" />
+          x
+          <input type="number" />
+        </div>
+
+        {/* DISPLAY */}
+        <div id="display"></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
